17b7a08c59fc622a24fe8faacd9ead7bb33814b8
Vaibhav Sharma <87849165+Vaibhav792000@users.noreply.github.com>
2024-02-24 11:34:52 +0530

    Data-types

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code in newer version
+
+// alert("hello World!");
+// alert(3+3);  //error because we are using Node.js...not browser
+console.log(3+3);
+console.log("Hitesh");
+
+
+let name =  "Hitesh"; // String
+let age = 18; // Int
+let is_LoggedIn = false; // Boolean
+let state;
+
+// Number  => 2^53
+// BigIntString =>
+//  Boolean
+//  null => Standalone value
+//  Symbol
+
+//  Object
+
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof is_LoggedIn);
+console.log(typeof undefined);
+console.log(typeof null);
