20d1440db32a0472d99c707e1743410439195f8a
Vaibhav Sharma <87849165+Vaibhav792000@users.noreply.github.com>
2024-02-24 11:34:10 +0530

    Variables

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId = 1234;
+let mail = "vaibhavsharma0200@gmail.com";
+var pass = "1234";
+let state = "stable";
+activity =  "jaipur";
+let accstate;
+
+
+
+console.log(accountId);
+console.table([accountId,mail,pass,state,activity,accstate]);
