1287c1d945b78d7df1d96826c77bdad7711be70a
Vaibhav Sharma <87849165+Vaibhav792000@users.noreply.github.com>
2024-02-26 18:24:43 +0530

    Conversion Operations

---
+++ conversion_Operations.js
@@ -0,0 +1,78 @@
+// let score = "33";
+let score = "33abc"
+let num = null;
+let abc = undefined;
+let bcd = true;
+let def = false;
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//conversion of String to Number
+
+// let valueInNumber = Number(score);
+// let value = Number(num);
+// let valueIn = Number(abc);
+// let valueInN = Number(bcd);
+// let valueInNum = Number(def);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(value);
+// console.log(valueIn);
+// console.log(valueInN);
+// console.log(valueInNum);
+
+// // "33" => 33
+// // "33abc" => NaN
+// // True => 1
+// // False => 0
+
+let is_LoggedIn = 1;
+ 
+let booleanIsLoggedIn = Boolean(is_LoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0 => False
+// "" => False
+// "Hitesh => true"
+
+let someNumber = 33;
+let stringNum = String(someNumber);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+
+
+// ****************************OPREATIONS**************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello ";
+let str2 = "Vaibhav";
+let str3 = str1+str2;
+// console.log(str3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let x = 3;
+const y = x++;
+console.log(x,y);
+
+let a = 3;
+const b = ++a;
+console.log(a,b);
